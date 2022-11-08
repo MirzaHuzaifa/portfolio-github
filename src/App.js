f6f3b19bd04b0ee24434ef57ea2625eb17df4d7b
huzaifa <mirzahuzaifa309@gmail.com>
2022-11-08 03:38:33 -0800

    add a new section called C

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
 
         <div className='Section-div-1'>A</div>
         <div className='Section-div-2'>B</div>
+        <div className='Section-div-3'>C</div>
         </div>
       </section>
     </div>
