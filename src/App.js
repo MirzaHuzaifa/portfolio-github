546b793fc2d2b23bc082fdc65327d9961ff2deda
huzaifa <mirzahuzaifa309@gmail.com>
2022-11-08 03:22:21 -0800

    portfolio github

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <section className='Section'>
+        <div className="Section-div">
+
+        <div className='Section-div-1'>A</div>
+        <div className='Section-div-2'>B</div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
