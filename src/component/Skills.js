87c61b1d58281dd562165133228e436200d1f316
Junaid <junaid96.pk@gmail.com>
2022-11-18 03:54:38 +0500

    set background image

--- src/component/Skills.js
+++ src/component/Skills.js
@@ -1,46 +1,42 @@
 import React from "react";
+import imgMountain from "./mountain.jpg";
 
 export default function Skills(){
-
     return(
+        <section style={{'backgroundImage': `url(${imgMountain})`}}>
 
-
-        
-        
-        <section>
-
-        <div className="w-11/12 pt-8 mx-auto max-w-screen-2xl bg-[url('/]mages/mountain.jpg')] md:w-10/12 xl:w-8/12">
+        <div className="w-11/12 pt-8 mx-auto max-w-screen-2xl md:w-10/12 xl:w-8/12">
             <h1 className="text-[#E63946] h-[100px] font-semibold text-[30px] text-center" >Services</h1>
 
             <div className="grid justify-between grid-cols-1 gap-6 m-auto sm:grid sm:gap-10 sm:grid-cols-3">
         
                 <div className="border h-[230px] pt-5 p-2 rounded-lg" style={{ boxshadow:"1px 1px 30px 2px rgb(240, 238, 238)"}}>
-                    <img src="images/monitor.png" alt="picture" className="w-[70px] h-[60px] m-auto"/>
+                    <img src="images/monitor.png" alt="" className="w-[70px] h-[60px] m-auto"/>
                     <h1 className="text-[#111827] text-[20px] text-center">Web Developement</h1>
                     <p className="text-[#111827] text-[14px] text-center pt-5">Lörem ipsum kötrebel askap polyll. Begisk prebaligt. Studpre balig prebalig
                     </p>
                 </div>
                 <div className="border h-[230px]  pt-5 p-2 rounded-lg" style={{ boxshadow:"1px 1px 30px 2px rgb(240, 238, 238)"}}>
-                    <img src="images/screen.png" alt="picture" className="w-[70px] h-[60px] m-auto"/>
+                    <img src="images/screen.png" alt="" className="w-[70px] h-[60px] m-auto"/>
                 <h1 className="text-[#111827] text-[20px] text-center">Web Design</h1>
                 <p className="text-[#111827] text-[14px] text-center pt-5 ">Lörem ipsum kötrebel askap polyll. Begisk prebaligt. Studpre balig prebali gpolyll. Begisk prebaligt. Studpre balig prebalig
         
                 </p>
             </div>
                 <div className="border h-[230px] pt-5 p-2 rounded-lg" style={{ boxshadow:"1px 1px 30px 2px rgb(240, 238, 238)"}}>
-                    <img src="images/mobile.png" alt="picture" className="w-[70px] h-[60px] m-auto"/>
+                    <img src="images/mobile.png" alt="" className="w-[70px] h-[60px] m-auto"/>
                 <h1 className="text-[#111827] text-[20px] text-center">App Developement</h1>
                 <p className="text-[#111827] text-[14px] text-center pt-5">Lörem ipsum kötrebel askap polyll. Begisk prebaligt. Studpre balig prebalig
                 </p>
                 </div>
                 <div className="border h-[230px] pt-5 p-2 rounded-lg" style={{ boxshadow:"1px 1px 30px 2px rgb(240, 238, 238)"}}>
-                    <img src="images/suqare.png" alt="picture" className="w-[70px] h-[60px] m-auto"/>
+                    <img src="images/suqare.png" alt="" className="w-[70px] h-[60px] m-auto"/>
                 <h1  className="text-[#111827] text-[20px] text-center">UI/UX Design</h1>
                 <p className="text-[#111827] text-[14px] text-center pt-5">Lörem ipsum kötrebel askap polyll. Begisk prebaligt. Studpre balig prebalig
                 </p>
                 </div>
                 <div className="border h-[230px] pt-5 p-2 rounded-lg" style={{ boxshadow:"1px 1px 30px 2px rgb(240, 238, 238)"}}>
-                    <img src="images/suqare.png" alt="picture" className="w-[70px] h-[60px] m-auto"/>
+                    <img src="images/suqare.png" alt="" className="w-[70px] h-[60px] m-auto"/>
                     <h1 className="text-[#111827] text-[20px] text-center ">UI/UX Design</h1>
                     <p className="text-[#111827] text-[14px] text-center pt-5">Lörem ipsum kötrebel askap polyll. Begisk prebaligt. Studpre balig prebalig
                     </p>
